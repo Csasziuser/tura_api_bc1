fa1ef36c9a8c72684d788aeb313ac7f9286f904f
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2026-02-05 11:03:51 +0100

    migrations and models

--- app/Models/Jelentkezes.php
+++ app/Models/Jelentkezes.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Jelentkezes extends Model
 {
-    //
+    protected $table = 'jelentkezesek';
+    protected $fillable = ['tura_id', 'email', 'letszam'];
+
+    public function tura(){
+        return $this->belongsTo(Tura::class);
+    }
 }
--- app/Models/Tura.php
+++ app/Models/Tura.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tura extends Model
 {
-    //
+    protected $table = 'turak';
+    protected $fillable = ['nev','tav','elerheto_hely'];
+
+    public function jelentkezesek() {
+        return $this->hasMany(Jelentkezes::class);
+    }
 }
