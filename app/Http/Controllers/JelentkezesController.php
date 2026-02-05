3a7c308138cf47e30db2a00879d3af7f049239a4
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2026-02-05 12:33:34 +0100

    jelentkezesek GET és POST

--- app/Http/Controllers/JelentkezesController.php
+++ app/Http/Controllers/JelentkezesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tura;
+use App\Models\Jelentkezes;
 
 class JelentkezesController extends Controller
 {
@@ -11,7 +13,8 @@ class JelentkezesController extends Controller
      */
     public function index()
     {
-        //
+        $jelentkezesek = Jelentkezes::with('tura')->get();
+        return response()->json($jelentkezesek,200,options:JSON_UNESCAPED_UNICODE);
     }
 
     /**
@@ -19,7 +22,32 @@ class JelentkezesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+       $request->validate([
+        'tura_id' => 'required|exists:turak,id',
+        'email' => 'required|email',
+        'letszam' => 'required|integer|min:1'
+       ]);
+
+       $tura = Tura::find($request->tura_id);
+
+       if ($tura) {
+        if ($request->letszam > $tura->elerheto_hely) {
+           return response()->json(['uzenet' => 'Nincs elég elérhető hely!'],400,options:JSON_UNESCAPED_UNICODE);
+        }
+        else {
+            // $tura->elerheto_hely -= $request->letszam;
+            // $tura->save();
+            $tura->decrement('elerheto_hely',$request->letszam);
+        }
+       }
+
+       Jelentkezes::create([
+        'tura_id' => $request->tura_id,
+        'email' =>$request->email,
+        'letszam' => $request->letszam
+       ]);
+
+       return response()->json(["uzenet" => "Sikeres jelentkezés!"],201,options:JSON_UNESCAPED_UNICODE);
     }
 
     /**
