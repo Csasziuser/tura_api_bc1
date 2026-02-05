3df8e5b40eb17974f159223ba01b0ce5f9712e83
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2026-02-05 11:17:22 +0100

    api.php done

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Models\Jelentkezes;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TuraController;
+use App\Http\Controllers\JelentkezesController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/turak',[TuraController::class, 'index']);
+Route::post('/turak',[TuraController::class, 'store']);
+
+Route::get('/jelentkezesek', [JelentkezesController::class,'index']);
+Route::post('jelentkezesek',[JelentkezesController::class,'store']);
+Route::delete('/jelentkezesek/{id}',[JelentkezesController::class, 'destroy']);
